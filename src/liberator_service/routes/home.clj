1c6c59e103229c890d4b2b510a538fc10616442d
Yury Kotov <bairkan@yandex.ru>
2015-07-14 19:00:45 +0300

    Resources complete

--- src/liberator_service/routes/home.clj
+++ src/liberator_service/routes/home.clj
@@ -1,9 +1,54 @@
 (ns liberator-service.routes.home
   (:require [compojure.core :refer :all]
-            [liberator-service.views.layout :as layout]))
+            [liberator.core
+             :refer [defresource resource request-method-in by-method]]
+            [cheshire.core :refer [generate-string]]
+            [noir.io :as io]
+            [clojure.java.io :refer [file]]))
 
-(defn home []
-  (layout/common [:h1 "Hello World!"]))
+(def users (atom ["John" "Jane"]))
+
+(defresource get-users
+  :allowed-methods [:get]
+  :handle-ok (fn [_] (generate-string @users))
+  :available-media-types ["application/json"])
+
+(defresource add-user
+  :allowed-methods [:post]
+  :post!
+  (fn [context]
+    (let [params (get-in context [:request :form-params])]
+      (swap! users conj (get params "user"))))
+  :handle-created (fn [_] (generate-string @users))
+  :available-media-types ["application/json"])
+
+(defresource users-resource
+  :malformed? (by-method
+                {:get nil :post (fn [context]
+                    (let [params (get-in context [:request :form-params])]
+                      (empty? (get params "user"))))})
+  :handle-malformed "user cannot be empty!"
+  :allowed-methods [:get :post]
+  :post!
+  (fn [context]
+    (let [params (get-in context [:request :form-params])]
+      (swap! users conj (get params "user"))))
+  :handle-created (fn [_] (generate-string @users))
+  :handle-ok (fn [_] (generate-string @users))
+  :available-media-types ["application/json"])
+
+(defresource home
+  :available-media-types ["text/html"]
+  :exists?
+  (fn [_]
+    (.exists (file (str (io/resource-path) "/home.html"))))
+  :handle-ok
+  (fn [_]
+    (clojure.java.io/input-stream (io/get-resource "/home.html")))
+  :last-modified
+  (fn [_]
+    (.lastModified (file (str (io/resource-path) "/home.html")))))
 
 (defroutes home-routes
-  (GET "/" [] (home)))
+  (ANY "/" request home)
+  (ANY "/users" request users-resource))
